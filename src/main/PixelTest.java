89996475eed323c1b683c02e00fe98765d765679
NovotJa5 <novot.jak@seznam.cz>
2018-09-25 19:16:39 +0200

    Initial commit

---
+++ src/main/PixelTest.java
@@ -0,0 +1,54 @@
+package main;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+
+public class PixelTest {
+
+    private JFrame window;
+    private JPanel panel;
+    private BufferedImage img;
+
+
+    public PixelTest() {
+
+        window = new JFrame();
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        window.setSize(800, 600);
+
+        panel = new JPanel() {
+            @Override
+            public void paint(Graphics g) {
+
+                super.paintComponent(g);
+                g.drawImage(img, 0, 0, null);
+            }
+        };
+        window.add(panel);
+
+        img = new BufferedImage(800, 600, BufferedImage.TYPE_INT_RGB);
+
+        window.setVisible(true);
+
+        drawPixel(100, 50, Color.GREEN.getRGB());
+
+        panel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                drawPixel(e.getX(), e.getY(), Color.YELLOW.getRGB());
+            }
+        });
+    }
+
+    private void drawPixel(int x, int y, int color) {
+        img.setRGB(x, y, color);
+        panel.getGraphics().drawImage(img, 0, 0, null);
+    }
+
+    public static void main(String[] args) {
+        new PixelTest();
+    }
+}
